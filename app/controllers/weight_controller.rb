0deb2bad0cf7978f90706f39613e63aa11be04a3
suzu1995 <suzu.chao@gmail.com>
2022-08-28 15:18:10 +0900

    ユーザの一覧表示まで作成

---
+++ app/controllers/weight_controller.rb
@@ -0,0 +1,2 @@
+class WeightController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'user#index'
+
+  get "user", to: "user#index"
+  get "user/:id", to: "user#show"
 end
